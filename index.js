29832083a89afd0b30ae662ca271b06aab96405e
unknown <dasilvadeoliveirafabio@gmail.com>
2022-07-10 15:54:01 -0300

    teste de deplay 3

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const  route  = require('./Route')
 const cors = require('cors')
 const app = express()
 app.use(cors())
-app.use(route)
+//app.use(route)
+app.get('/',(req,res)=>{
+    res.send('teste')
+})
 const PORT = process.env.PORT || 4000;
 app.listen(PORT,()=>console.log('posta 4000'))
